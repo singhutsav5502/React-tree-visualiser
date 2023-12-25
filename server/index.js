8df7726454173ca8a87b406b53607b52e2d3b583
Utsav Singh <singhutsavofficial@gmail.com>
2023-12-25 23:25:58 +0530

    add try catch

--- server/index.js
+++ server/index.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 app.post('/parse', bodyParser.text(), (req, res) => {
-    const { nodes, edges } = parseJSXContent(req.body.fileContent, req.body.fileName, req.body.fileID);
-    res.send({ nodes, edges });
-})
+    try {
+        const { nodes, edges } = parseJSXContent(req.body.fileContent, req.body.fileName, req.body.fileID);
+        res.send({ nodes, edges });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
