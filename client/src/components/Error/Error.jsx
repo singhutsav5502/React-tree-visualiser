b1b983f7a1a2e1fe9b3283410683a2dac22c73df
Utsav Singh <singhutsavofficial@gmail.com>
2023-12-26 22:04:48 +0530

    refactor About section and add Error message

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -9,9 +9,9 @@ const Error = ({  setIsErr }) => {
             <div className="error-wrapper">
                 <div className="error-internal-wrapper">
 
-                    <h1 className='font-NeueHaas' style={{ marginBottom: '1.25rem' }}>Error</h1>
-                    <p style={{ fontSize: '0.9rem', fontWeight: '300' }}>There seems to be an error while parsing the file!</p>
-                    <p style={{ fontSize: '0.9rem', fontWeight: '300' }}>Please check and make sure that you are using a <strong>js/jsx</strong> file that contains JSX syntax</p>
+                    <h1 className='font-NeueHaas' style={{ marginBottom: '1.25rem' , color:'var(--font-primary)'}}>Error</h1>
+                    <p style={{ fontSize: '0.9rem', fontWeight: '300' , color:'var(--font-primary)'}}>There seems to be an error while parsing the file!</p>
+                    <p style={{ fontSize: '0.9rem', fontWeight: '300' , color:'var(--font-primary)'}}>Please check and make sure that you are using a <strong>js/jsx</strong> file that contains JSX syntax</p>
                     {/* reuse existing button class */}
                     <span className="file-header-buttons-wrapper" style={{alignSelf:'flex-end' , marginTop:'1.5rem'}}>
                         <button style={{padding:'10px 15px 10px 15px' , marginLeft:'0'}} onClick={okClickHandler}>OK</button>
